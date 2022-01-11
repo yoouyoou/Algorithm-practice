7a70edc18cc704784fcc2031b80e24ca6cc329ce
yoouyoou <yny026@gmail.com>
2022-01-11 11:21:07 +0900

    불켜기 풀이

--- coding_study_12/src/b_light_up/b_11967.java
+++ coding_study_12/src/b_light_up/b_11967.java
@@ -2,6 +2,8 @@ package b_light_up;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 // 골드3
@@ -9,11 +11,13 @@ public class b_11967 {
 	
 	static int count = 0;
 	static int N;
-	static int[] moveX = {-1, 1, 0, 0};	//상하좌우
-	static int[] moveY = {0, 0, -1, 1};
-	static boolean[][] grid, visited;
+	static int[] moveX = {0,0,1,-1};		//상하좌우
+	static int[] moveY = {1,-1,0,0};
+	static boolean[][] grid, visited;		//현재 간곳까지 켤 수 있는 배열, 
 	static ArrayList<String>[] list;
+	static ArrayList<String> waiting = new ArrayList<>();
 	static HashMap<String, Integer> map;
+	static Queue<String> q = new LinkedList<>();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -47,42 +51,23 @@ public class b_11967 {
 		}
 		
 		grid[1][1] = true;
-		DFS(1, 1);
+		DFS(1,1);
 		
+		/*최종 확인 출력
 		for(int i = 1; i <= N; i++) {
 			for(int j = 1; j <= N; j++)
 				System.out.print(grid[i][j] + " ");
 			System.out.println();
-		}
-		
-		
-		System.out.println(count);
+		}*/
+			
+		System.out.println(count+1);
 		sc.close();
 	}
 
-	// 현재 방에서 킬 수 있는 불 다 켜기
-	public static void current_light(int x, int y) {
-		if(map.containsKey(x+","+y)) {
-			for(String next : list[map.get(x+","+y)]) {
-				int nextX = Integer.parseInt(next.split(",")[0]);
-				int nextY = Integer.parseInt(next.split(",")[1]);
-				if(nextX > 0 && nextX <= N && nextY > 0 && nextY <= N) {
-					grid[nextX][nextY] = true;
-				}
-			}
-		}
-		for(int i = 1; i <= N; i++) {
-			for(int j = 1; j <= N; j++)
-				System.out.print(grid[i][j] + " ");
-			System.out.println();
-		}
-		System.out.println();
-	}
-	
+	// 해당 방에서 상하좌우 확인하고 갈 수 있는 곳 가서 불켜기
 	public static void DFS(int x, int y) {
-		System.out.println(x + " " +y);
-		if(grid[x][y] == false || visited[x][y] == true)	//불이 꺼져 있거나 이미 방문했던 곳이라면
-			return;											//바로 리턴
+		if(visited[x][y] == true)					// 이미 방문했던 곳이라면
+			return;									//바로 리턴
 		
 		visited[x][y] = true;	//방문표시
 		current_light(x, y);	//여기서 켤 수 있는 방의 불 모두 켜기
@@ -90,29 +75,34 @@ public class b_11967 {
 		for(int k = 0; k < 4; k++) {
 			int nx = x + moveX[k];
 			int ny = y + moveY[k];
-			if(nx > 0 && nx <= N && ny > 0 && ny <= N && grid[nx][ny]) //움직일 수 있는 곳이 true여야 함-불켜져있어야
-				DFS(nx, ny);
+			if(nx > 0 && nx <= N && ny > 0 && ny <= N && !visited[nx][ny]) {
+				if(grid[nx][ny]) //움직일 수 있는 곳이 true여야 함-불켜져있어야
+					DFS(nx, ny);
+				else
+					waiting.add(nx+","+ny);
+			}
+		}
+		
+		for(int i = 0; i < waiting.size(); i++) {
+			int a = Integer.parseInt(waiting.get(i).split(",")[0]);
+			int b = Integer.parseInt(waiting.get(i).split(",")[1]);
+			if(grid[a][b] && !visited[a][b])
+				DFS(a,b);
 		}
 	}
 	
-	/*
-	public static void DFS(int x, int y) {
-		//System.out.println("x: " + x + " y :" + y);
-		if(grid[x][y])
-			return;
-		
-		grid[x][y] = true;
-		count++;
-		
+	// 현재 방에서 킬 수 있는 불 다 켜기
+	public static void current_light(int x, int y) {
 		if(map.containsKey(x+","+y)) {
 			for(String next : list[map.get(x+","+y)]) {
 				int nextX = Integer.parseInt(next.split(",")[0]);
 				int nextY = Integer.parseInt(next.split(",")[1]);
-				if(nextX > 0 && nextX <= N && nextY > 0 && nextY <= N)
-					DFS(nextX, nextY);
+				if(!grid[nextX][nextY]) {
+					//System.out.println(nextX+" "+nextY+" 카운트 증가");
+					grid[nextX][nextY] = true;
+					count++;
+				}
 			}
 		}
 	}
-	*/
-	
 }
