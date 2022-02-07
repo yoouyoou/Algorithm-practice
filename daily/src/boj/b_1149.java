88c0c0fea79ae69fc2c9e8a399ffd3cb91214edf
yoouyoou <yny026@gmail.com>
2022-02-07 23:55:47 +0900

    rgb거리 입력받기

---
+++ daily/src/boj/b_1149.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// RGB거리
+public class b_1149 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] cost = new int[N][N];
+		
+		//각 RGB칠하는 비용
+		for(int i = 0; i < N; i++) {
+			String[] str = br.readLine().split(" ");
+		}
+	}
+
+}
