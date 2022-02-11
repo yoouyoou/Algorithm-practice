1c554cbb4ef4d552b49d40942e070621c71f6aa6
yoouyoou <yny026@gmail.com>
2022-02-11 18:02:18 +0900

    백준 rgb거리_dp이용

--- daily/src/boj/b_1149.java
+++ daily/src/boj/b_1149.java
@@ -6,48 +6,43 @@ import java.io.InputStreamReader;
 
 // RGB거리
 public class b_1149 {
-
-	static int N;
-	static int min = Integer.MAX_VALUE;
-	static int[][] cost;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		cost = new int[N][3];
-		int[] arr = new int[N];
-		boolean[] visited = new boolean[N];
-		boolean[] rgb = new boolean[3];
+		int N = Integer.parseInt(br.readLine());
+		int[][] cost = new int[N][3];
+		int[][] dp = new int[N][3];
 		
-		//각 RGB칠하는 비용
+		//cost입력 받기
 		for(int i = 0; i < N; i++) {
 			String[] str = br.readLine().split(" ");
 			for(int j = 0; j < 3; j++)
 				cost[i][j] = Integer.parseInt(str[j]);
 		}
 		
-	}
-	
-	public static void dfs(int depth, int[] arr, boolean[] visited, boolean[] rgb) {
-		if(depth == N) {
-			if(arr[depth] < min)
-				min = arr[depth];
-			return;
-		}
-		
+		//dp생성
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < 3; j++) {
-				if(!rgb[j]) {
-					rgb[j] = true;
-					arr[depth] = cost[i][j];
-					visited[i] = true;
-					dfs(depth+1, arr, visited, rgb);
-					visited[i] = false;
-					rgb[j] = false;
+				if(i == 0)
+					dp[i][j] = cost[i][j];
+				else {
+					if(j == 0)	//r칠하는 경우
+						dp[i][j] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][j];
+					else if(j == 1)	//g칠하는 경우
+						dp[i][j] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][j];
+					else if(j == 2)	//b칠하는 경우
+						dp[i][j] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][j];
 				}
 			}
 		}
 		
+		int min = Integer.MAX_VALUE;
+		for(int i = 0; i < 3; i++) {
+			if(dp[N-1][i] < min)
+				min = dp[N-1][i];
+		}
+		
+		System.out.println(min);
 	}
-
+	
 }
