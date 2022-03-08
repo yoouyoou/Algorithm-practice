ada07f29ca299fa9937be585fc0a2ca61813f046
yoouyoou <yny026@gmail.com>
2022-03-08 23:56:49 +0900

    자물쇠와 열쇠_배열과 좌표이용해서 풀어보는 중

--- daily/src/programmers/자물쇠와열쇠.java
+++ daily/src/programmers/자물쇠와열쇠.java
@@ -1,27 +1,50 @@
 package programmers;
 
-public class 자물쇠와열쇠 {
+import java.util.ArrayList;
 
+public class 자물쇠와열쇠 {
+	
 	public static void main(String[] args) {
 		int[][] key = {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}};
 		int[][] lock = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-		
+
 		System.out.println(solution(key, lock));
 	}
 	
+	//그냥 무한 반복
 	public static boolean solution(int[][] key, int[][] lock){
-		int M = key.length;		//key한 변의 길이
-		int N = lock.length;	//lock한 변의 길이
-		int maxIdx = N-M;	// (N-1) - (M-1) 최대 갈 수 있는 인덱스 값
+		ArrayList<Point> lockList = new ArrayList<>();
+		ArrayList<Point> keyList = new ArrayList<>();
 		
-		for(int i = 0; i < maxIdx; i++) {
-			for(int j = 0; j < maxIdx; j++) {
-				if(key[j] == lock[j])
-					break;
-			}
-		}
-				
-		return false;
+		for(int i = 0; i < lock.length; i++)
+			for(int j = 0; j < lock.length; j++)
+				if(lock[i][j] == 0)	//홈이라면
+					lockList.add(new Point(i, j));
+		
+		for(int i = 0; i < key.length; i++)
+			for(int j = 0; j < key.length; j++)
+				if(key[i][j] == 1)	//돌기 부분이라면
+					keyList.add(new Point(i, j));
+		
+		return true;
 	}
 
+}
+
+
+//좌표 클래스 		// list정렬하기
+class Point implements Comparable<Point>{
+	int r, c;
+	
+	public Point(int r, int c) {
+		this.r = r;
+		this.c = c;
+	}
+
+	@Override
+	public int compareTo(Point arg0) {
+		if(this.r < arg0.r)
+			return -1;
+		return 0;
+	}
 }
