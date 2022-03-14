75bff0451230ebbd69d2cb125386f2feda7be3a4
yoouyoou <yny026@gmail.com>
2022-03-14 21:57:11 +0900

    백준 1003번

--- study_DP/src/study_DP/피보나치함수.java
+++ study_DP/src/study_DP/피보나치함수.java
@@ -7,18 +7,36 @@ public class 피보나치함수 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int zero = 0, one = 0;
 		int T = sc.nextInt();	//테스트 케이스 개수
+		
+		int max = 0;
 		int[] tarr = new int[T];
-		int[][] dp = new int[T][T];
+		for(int i = 0; i < T; i++) {
+			tarr[i] = sc.nextInt();	//테스트 예제 담기
+			if(tarr[i] > max)
+				max = tarr[i];
+		}
 		
-		dp[0][0] = 0;
-		dp[0][1] = 1;
-		for(int i = 0; i < T; i++)
-			tarr[i] = sc.nextInt();
+		int[][] dp = new int[max+1][2];		//0횟수, 1횟수를 담기 위해 2차원 배열
 		
-//		for(int i = 0; i < tarr.length; i++)
+		//F[0] = 0
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		
+		//F[1] = 1
+		dp[1][0] = 0;
+		dp[1][1] = 1;
+		
+		for(int i = 2; i <= max; i++) {
+			dp[i][0] = dp[i-1][0] + dp[i-2][0];
+			dp[i][1] = dp[i-1][1] + dp[i-2][1]; 
+		}
+		
+		for(int i = 0; i < tarr.length; i++) {
+			System.out.println(dp[tarr[i]][0] + " " + dp[tarr[i]][1]);
+		}
 
+		sc.close();
 	}
 
 }
