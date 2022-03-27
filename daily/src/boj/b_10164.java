a3bab684b2391f94e1caa551ec7c440d196521e3
유나연 <yny026@gmail.com>
2022-03-27 23:58:49 +0900

    백준 10164번 dp이용

--- daily/src/boj/b_10164.java
+++ daily/src/boj/b_10164.java
@@ -5,34 +5,41 @@ import java.util.Scanner;
 //격자상의 경로(실버1)
 public class b_10164 {
 
+	static int[][] dp;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int K = sc.nextInt();
 		
+		dp = new int[N+1][M+1];
+		dp[1][1] = 1;
+		
 		if(K == 0) {	//꼭 거쳐가지 않아도 되는 경우
-			System.out.println(factorial(N + M - 2) / factorial(N-1) / factorial(M-1));
+			for(int i = 1; i <= N; i++) {
+				for(int j = 1; j <= M; j++) {
+					if(i == 1 && j == 0)
+						continue;
+					dp[i][j] = i < 0 ? 0 : dp[i][j] + j < 0 ? 0 : dp[i][j];
+				}
+			}
 		}
 		else {
 			int r = K / M + 1;
 			int c = K % M;
-			//System.out.println(r + " " + c);
-			int temp = factorial(c + r - 2) / factorial(c-1) / factorial(r-1);
-			int temp2 = factorial(M - c + N - r) / factorial(M-c) / factorial(N-r);
-			System.out.println(temp * temp2);
+			
+			for(int i = 1; i <= r; i++) {
+				for(int j = 1; j <= c; j++) {
+					if(i == 1 && j == 1)
+						continue;
+					dp[i][j] = i < 0? 0:dp[i][j] + j<0?0:dp[i][j];
+				}
+			}
 		}
 		
+		System.out.println(dp[N][M]);
 		sc.close();
-	}
-	
-	public static int factorial(int n) {
-		int rslt = 1;
-		
-		for(int i = 1; i <= n; i++)
-			rslt *= i;
-		
-		return rslt;
 	}
 
 }
