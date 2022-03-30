0f69b51f7c7ee9c4622d157b7b150490dbfbd6c4
유나연 <yny026@gmail.com>
2022-03-30 23:38:31 +0900

    백준 10164번

--- daily/src/boj/b_10164.java
+++ daily/src/boj/b_10164.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 //격자상의 경로(실버1)
 public class b_10164 {
-
-	static int[][] dp;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,29 +11,70 @@ public class b_10164 {
 		int M = sc.nextInt();
 		int K = sc.nextInt();
 		
-		dp = new int[N+1][M+1];
-		dp[1][1] = 1;
+		int[][] dp = new int[N+1][M+1];
+		int answer = 0;
+		boolean isVisited = false;
 		
-		if(K == 0) {	//꼭 거쳐가지 않아도 되는 경우
+		//경유지 없는 경우
+		if(K == 0) {
 			for(int i = 1; i <= N; i++) {
 				for(int j = 1; j <= M; j++) {
-					if(i == 1 && j == 0)
-						continue;
-					dp[i][j] = i < 0 ? 0 : dp[i][j] + j < 0 ? 0 : dp[i][j];
+					if(i == 1)
+						dp[i][j] = 1;
+					else if(j == 1)
+						dp[i][j] = 1;
+					else
+						dp[i][j] += dp[i-1][j] + dp[i][j-1];
 				}
 			}
 		}
+		//경유지 있는 경우
 		else {
-			int r = K / M + 1;
-			int c = K % M;
+			int r, c;
+//			int r = (K-1) / M + 1;
+//			int c = (K-1) % M + 1;
 			
-			for(int i = 1; i <= r; i++) {
-				for(int j = 1; j <= c; j++) {
-					if(i == 1 && j == 1)
-						continue;
-					dp[i][j] = i < 0? 0:dp[i][j] + j<0?0:dp[i][j];
+			if(K % M == 0) {
+				r = K / M;
+				c = M;
+			}else {
+				r = K / M + 1;
+				c = K % M;	
+			}
+			
+			for(int i = 1; i <= N; i++) {
+				if(isVisited)
+					break;
+				for(int j = 1; j <= M; j++) {
+					if(i == 1)
+						dp[i][j] = 1;
+					else if(j == 1)
+						dp[i][j] = 1;
+					else
+						dp[i][j] += dp[i-1][j] + dp[i][j-1];
+					
+					if(i == r && j == c) {
+						answer = dp[r][c];
+						dp[r][c] = 1;
+						isVisited = true;
+						break;
+					}	
+				}
+				
+			}
+			
+			for(int i = r; i <= N; i++) {
+				for(int j = c; j <= M; j++) {
+					if(i == r)
+						dp[i][j] = 1;
+					else if(j == c)
+						dp[i][j] = 1;
+					else
+						dp[i][j] += dp[i-1][j] + dp[i][j-1];
 				}
 			}
+			
+			dp[N][M] *= answer;
 		}
 		
 		System.out.println(dp[N][M]);
