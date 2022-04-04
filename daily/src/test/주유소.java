2f357e83f54e08563df1c770dfc33373c1519918
유나연 <yny026@gmail.com>
2022-04-04 17:56:40 +0900

    모의코테 재풀이

--- daily/src/test/주유소.java
+++ daily/src/test/주유소.java
@@ -8,19 +8,29 @@ public class 주유소 {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();		//도시개수
-		int[] cost = new int[N];	//리터당 가격
-		int[] dist = new int[N-1];	//도시간 거리
-		int totalL = 0, answer = 0;
+		long[] cost = new long[N];	//리터당 가격
+		long[] dist = new long[N-1];	//도시간 거리
 		
-		for(int i = 0; i < N-1; i++) {
+		//거리 입력
+		for(int i = 0; i < N-1; i++)
 			dist[i] = sc.nextInt();
-			totalL += dist[i];
-		}
 		
+		//비용 입력
 		for(int i = 0; i < N; i++)
 			cost[i] = sc.nextInt();
 		
+		//solution
+		int costIdx = 0;
+		long ans = 0;
+		long minCost = cost[costIdx];
+		for(int i = 0; i < N-1; i++) {
+			ans += minCost * dist[i];
+			if(minCost > cost[++costIdx])
+				minCost = cost[costIdx];
+		}
 
+		System.out.println(ans);
+		sc.close();
 	}
 
 }
--- daily/src/test/트리의지름.java
+++ daily/src/test/트리의지름.java
@@ -1,42 +1,73 @@
 package test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-//다익스트라?
 public class 트리의지름 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static ArrayList<Node>[] list;
+	static boolean[] visited;
+	static int node, max = 0;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int v = sc.nextInt();
-		int[][] tree = new int[v+1][v+1];
-		int[] dist = new int[v+1];
+		int n = Integer.parseInt(br.readLine());
+		list = new ArrayList[n+1];
+		
+		for(int i = 1; i <= n; i++)
+			list[i] = new ArrayList<Node>();
 		
 		//input
-		for(int i = 0; i < v; i++) {
-			int v1 = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			String[] str = br.readLine().split(" ");
+			int t = 0;
+			int v1 = Integer.parseInt(str[t++]);
 			while(true) {
-				int v2 = sc.nextInt();
-				if(v2 == -1)
+				if(str[t].equals("-1"))
 					break;
-				int distance = sc.nextInt();
-				tree[v1][v2] = distance;
+				int v2 = Integer.parseInt(str[t++]);
+				int cost = Integer.parseInt(str[t++]);
+				list[v1].add(new Node(v2, cost));
 			}
 		}
-
-		for(int i = 1; i <= v; i++) {
-			dist[i] = Integer.MAX_VALUE;	//최대값으로 설정
-		}
 		
-		//solution  다익스트라? dp?
-		for(int i = 1; i <= v; i++) {
-			for(int j = 1; j <= v; j++)
-				dist[j] = tree[i][j];
-		}
+		//solution
+		visited = new boolean[n+1];
+		dfs(1, 0);	//1과 가장 먼 노드
+		
+		visited = new boolean[n+1];
+		dfs(node, 0);
+		
+		System.out.println(max);
+		br.close();
 	}
 	
-	public static void dijkstra() {
+	public static void dfs(int x, int len) {
+		if(len > max) {
+			max = len;
+			node = x;
+		}
 		
+		visited[x] = true;
+		
+		for(int i = 0; i < list[x].size(); i++) {
+			Node n = list[x].get(i);
+			if(!visited[n.v]) {
+				dfs(n.v, n.cost + len);
+			}
+		}
 	}
 
+}
+
+class Node{
+	int v, cost;
+	
+	public Node(int v, int cost) {
+		this.v = v;
+		this.cost = cost;
+	}
 }
