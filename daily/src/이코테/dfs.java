33a6ab8d7634f52645101e343fa43a8b851e2415
유나연 <yny026@gmail.com>
2022-04-14 10:50:35 +0900

    [bfs]미로 탈출

--- daily/src/이코테/dfs.java
+++ daily/src/이코테/dfs.java
@@ -1,44 +1,62 @@
 package 이코테;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class dfs {
 
 	static int[] dr = {-1, 1, 0, 0};	//상하좌우
 	static int[] dc = {0, 0, -1, 1};
-	static int n, m;
+	static int n, m, ans = 0;
 	static boolean[][] visited;
-	static int[][] map = {{0,0,1,1,0},
-			{0,0,0,1,1},
-			{1,1,1,1,1},
-			{0,0,0,0,0}};;
+	static int[][] map = { {0, 0,0,0,0,0,0},
+			{0, 1,0,1,0,1,0},
+			{0, 1,1,1,1,1,1},
+			{0, 0,0,0,0,1,1},
+			{0, 1,1,1,1,1,1},
+			{0, 1,1,1,1,1,1}};
 	
 	public static void main(String[] args) {
-		n = 4;
-		m = 5;
+		n = 5;
+		m = 6;
 		visited = new boolean[n][m];
 		
-		int ans = 0;
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				if(!visited[i][j] && map[i][j] == 0) {
-					dfs(i, j);
-					ans++;
-				}
-			}
-		}
-
-		System.out.println(ans);
+		bfs(1,1);
+		System.out.println(map[n][m]);
 	}
 	
-	public static void dfs(int r, int c) {
-		visited[r][c] = true;
+	public static void bfs(int r, int c) {
+		Queue<Node> q = new LinkedList<Node>();
+		q.offer(new Node(r, c));
 		
-		for(int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if(nr >= 0 && nr < n && nc >=0 && nc < m)
-				if(!visited[nr][nc] && map[nr][nc]==0)
-					dfs(nr, nc);
+		while(!q.isEmpty()) {
+			Node node = q.poll();
+			
+			if(node.r == n && node.c == m)
+				return;
+			
+			for(int i = 0; i < 4; i++) {
+				int nr = node.r + dr[i];
+				int nc = node.c + dc[i];
+				
+				if(nr>0 && nr <= n && nc > 0 && nc <= m) {
+					if(map[nr][nc] == 1) {
+						q.offer(new Node(nr, nc));
+						map[nr][nc] += map[node.r][node.c];
+					}
+				}
+			}
+			
 		}
+		
 	}
 
+}
+
+class Node {
+	int r, c;
+	Node(int r, int c){
+		this.r = r;
+		this.c = c;
+	}
 }
