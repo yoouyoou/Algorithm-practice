78d0ec4c7b3809adca4257726072e5e5c02cffed
유나연 <yny026@gmail.com>
2022-04-15 12:55:13 +0900

    [dp] 1로 만들기

--- daily/src/이코테/dp.java
+++ daily/src/이코테/dp.java
@@ -3,18 +3,25 @@ package 이코테;
 public class dp {
 
 	public static void main(String[] args) {
-		int N = 4; //식량 창고 개수
-		int[] food = {1, 3, 1, 5};
-		int[] dp = new int[N];
+		int N = 26;
+		int[] dp = new int[N+1];	//i를 1로 만들기 위한 최소 연산횟수
 		
-		dp[0] = food[0];
-		dp[1] = food[1];
-		for(int i = 2; i < N; i++) {
-			dp[i] = Math.max(dp[i-1], dp[i-2] + food[i]);
+		dp[1] = 0; // 1인 경우 연산을 수행할 필요가 없기 때문에
+		
+		for(int i = 2; i <= N; i++) {
+			dp[i] = dp[i-1] + 1;	//현재 수에서 1을 빼서 나온 연산횟수
+			
+			if(i % 2 == 0)
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
+			
+			if(i % 3 == 0)
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
+			
+			if(i % 5 == 0)
+				dp[i] = Math.min(dp[i], dp[i/5] + 1);
 		}
 		
-		System.out.println(dp[N-1]);
-
+		System.out.println(dp[N]);
 	}
 
 }
