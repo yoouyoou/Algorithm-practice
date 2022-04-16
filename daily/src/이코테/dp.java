9e7e38435e2414f1392c28d496aa047b258281cc
유나연 <yny026@gmail.com>
2022-04-16 15:22:20 +0900

    [dp]병사 배치하기

--- daily/src/이코테/dp.java
+++ daily/src/이코테/dp.java
@@ -1,55 +1,37 @@
 package 이코테;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class dp {
 	
 	public static void main(String[] args) {
-		//testcase1 -> 19
-		int n = 3;
-		int m = 4;
-		int[][] map = {{1, 3, 3, 2},
-				{2, 1, 4, 1},
-				{0, 6, 4, 7}};
+		Scanner sc = new Scanner(System.in);
 		
-		//testcase2 -> 16
-//		int n = 4;
-//		int m = 4;
-//		int[][] map = {{1, 3, 1, 5},
-//				{2, 2, 4, 1},
-//				{5, 0, 2, 3},
-//				{0, 6, 1, 2}};
+		int N = 7;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < N; i++)
+			list.add(sc.nextInt());
+//		int[] arr = {15, 11, 4, 8, 5, 2, 4};
 		
-		int[][] dp = new int[n][m];
-		for(int i = 0; i < n; i++)
-			dp[i][0] = map[i][0];
+		int[] dp = new int[N];
+		Collections.reverse(list);
+		Arrays.fill(dp, 1);
 		
-		for(int i = 1; i < m; i++) {		//열 검사
-			for(int j = 0; j < n; j++) {	//행 검사
-				int leftUp=0, leftDown=0, left=0;
-				
-				//[j-1][i-1] //[j][i-1]  //[j+1][i-1]
-				if(j-1 >= 0 && j-1 < n)
-					leftUp = dp[j-1][i-1];
-				
-				if(j >= 0 && j < n)
-					left = dp[j][i-1];
-				
-				if(j+1 >= 0 && j+1 < n)
-					leftDown = dp[j+1][i-1];
-				
-				dp[j][i] = Math.max(leftUp, Math.max(leftDown, left)) + map[j][i];
-			}
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i; j++)
+				if(list.get(j) < list.get(i))
+					dp[i] = Math.max(dp[i], dp[j]+1);
 		}
 		
 		int ans = 0;
-		for(int i = 0; i < n; i++) {
-			if(ans < dp[i][m-1])
-				ans = dp[i][m-1];
-		}
+		for(int i = 0; i < N; i++)
+			ans = Math.max(ans, dp[i]);
 		
-		System.out.println(ans);
+		System.out.println(N - ans);
 	}
+	
 
 }
