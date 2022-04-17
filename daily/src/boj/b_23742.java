46bbc75a406e476cdc5f6e5ef0177e8006baba7e
유나연 <yny026@gmail.com>
2022-04-18 01:14:46 +0900

    백준 23742번 Player-based Team Distribution

--- daily/src/boj/b_23742.java
+++ daily/src/boj/b_23742.java
@@ -3,40 +3,48 @@ package boj;
 import java.util.Arrays;
 import java.util.Scanner;
 
+//Player-based Team Distribution
 public class b_23742 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int N = sc.nextInt();
-		int[] arr = new int[N];
+		long[] parr = new long[N]; 		  //양수 넣는 배열
+		long[] narr = new long[N]; 		  //음수 넣는 배열
+		int pidx = 0, nidx = 0, pCnt;	  //각 배열 인덱스, 양수배열 개수 카운트 변수	
+		long ans = 0, nsum = 0, psum = 0; 			  //플레이어 점수합, 음수 총합, 양수 총합
 		
-		int num = 0;
+		//input
 		for(int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-			if(arr[i] > 0)
-				num++;
+			int t = sc.nextInt();
+			if(t < 0) {
+				narr[nidx++] = t;
+				nsum += t;
+			}
+			else {
+				parr[pidx++] = t;
+				psum += t;
+			}
 		}
 		
-		int ans = 0;
+		//초기 점수합 구하기
+		ans = (psum * pidx) + nsum;
 		
-		//만약 모두 양수일 경우
-		if(num == N) {
-			Arrays.sort(arr);
-			for(int i = 0; i < N-1; i++)
-				ans += num * arr[i];
-//			ans += arr[N-1];
-		}
-		//음수가 하나라도 있으면
-		else {
-			for(int a: arr) {
-				if(a < 0)		//음수면 혼자 팀하게 냅둠
-					ans += a;
-				else
-					ans += num * a;
-			}	
+		Arrays.sort(narr, 0, nidx);		//음수배열 들어있는 만큼 정렬
+		
+		//점수합 갱신하기
+		pCnt = pidx;	//양수배열 총개수
+		for(int i = nidx-1; i >= 0; i--) {	//음수를 하나씩 양수쪽으로 넘김
+			pCnt++;
+			nsum -= narr[i];				//개인 음수팀 합 : 나중에 총합에서 뺄 것
+			psum += narr[i];
+			
+			ans = Math.max(ans, psum * pCnt + nsum);
 		}
 		
 		System.out.println(ans);
+		sc.close();
 	}
 
 }
