416f59f74ad3fa0c0014c26ef28197be6117a1c1
유나연 <yny026@gmail.com>
2022-05-06 17:05:20 +0900

    백준 1744번

--- daily2/src/study/그리디.java
+++ daily2/src/study/그리디.java
@@ -1,35 +1,62 @@
 package study;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-//게임을 만든 동준이
+//수 묶기
 public class 그리디 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int ans = 0;
-		int N = sc.nextInt();		//레벨의 수
-		int[] score = new int[N];	//각레벨을 클리어하면 얻는 점수
-		for(int i = 0; i < N; i++)	
-			score[i] = sc.nextInt();
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++)
+			arr[i] = sc.nextInt();
 		
-		//1 2 3 4단계
-		//5 3 7 5
-		//5 5 5 
-//		for(int i = 0; i < N-1; i++) {
-//			if(score[i] >= score[i+1])
-//				ans += score[i] - score[i+1] + 1;
-//		}
+		//-1 2 1 3
+		int i = N - 1;
+		Arrays.sort(arr);
+		for(i = N-1; i > 0; i--) {
+			
+			if(arr[i] <= 0)
+				break;
+			
+			if(arr[i] > 1 && arr[i-1] > 1) {		//둘다 양수
+				ans += arr[i] * arr[i-1];
+				i--;
+			}
+			else
+				ans += arr[i];
+		}
 		
-		for(int i = N-1; i > 0; i--) {
-			if(score[i-1] >= score[i]) {
-				int temp = score[i-1] - score[i] + 1;
-				ans += temp;
-				score[i-1] -= temp;
+		if(i == 0)
+			ans += arr[0];
+		else {
+			for(int j = 0; j <= i; j++) {
+				if(j == i) {
+					ans += arr[j];
+					break;
+				}
+				ans += arr[j] * arr[j+1];
+				j++;
 			}
 		}
 		
+//		if(arr[0] < 0 && arr[1] < 0) {
+//			for(int i = 2; i < N; i++)
+//				ans += arr[i];
+//			ans += arr[0] * arr[1];
+//		}
+//		else {
+//			for(int i = 0; i < N-2; i++)
+//				ans += arr[i];
+//			ans += arr[N-1] * arr[N-2];
+//		}
+		
 		System.out.println(ans);
+		sc.close();
 	}
 
 }
