1719cebb9463feb6e7819f88fdf976fe69c83d50
유나연 <yny026@gmail.com>
2022-06-08 23:56:57 +0900

    백준 친구비

--- daily2/src/boj/b_16562.java
+++ daily2/src/boj/b_16562.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 //친구비
 public class b_16562 {
 
-	static int[] parent, A;
+	static int[] parent, A, cost;
 	static boolean[] visited;
 	static int minCost = 0;
 	
@@ -19,84 +19,68 @@ public class b_16562 {
 		int N = Integer.parseInt(str[0]);	//학생 수
 		int M = Integer.parseInt(str[1]);	//친구관계 수
 		int k = Integer.parseInt(str[2]);	//가지고 있는 돈
-		
-		Friend[] arr = new Friend[M];
 		int ans = 0;
-		boolean flag = false;
 		
+		A = new int[N+1];
 		visited = new boolean[N+1];
 		parent = new int[N+1];
-		for(int i = 1; i <= N; i++)
-			parent[i] = i;
+		cost = new int[N+1];
 		
 		str = br.readLine().split(" ");
-		A = new int[N+1];
+		
+		for(int i = 1; i <= N; i++)
+			parent[i] = i;			//부모노드 초기화
+		
 		for(int i = 1; i <= N; i++)
 			A[i] = Integer.parseInt(str[i-1]);	//친구비
+		
+		Arrays.fill(cost, 10001);
 		
 		for(int i = 0; i < M; i++) {
 			str = br.readLine().split(" ");
 			int v = Integer.parseInt(str[0]);
 			int w = Integer.parseInt(str[1]);
 			
-			arr[i] = new Friend(v, w);		
+			union(v, w);
 		}
 		
-		Arrays.sort(arr);
+		for(int i = 1; i <= N; i++) {
+			int p = find(i);
+			cost[p] = Math.min(cost[p], A[i]);
+		}
 		
-		for(int i = 0; i < M; i++) {
-			int v = arr[i].x;
-			int w = arr[i].y;
-			
-			if(visited[v] == false && visited[w] == false) {
-				if(i == M-1) {
-					ans += Math.min(A[v], A[w]);
-					break;
-				}
-				
-				if(flag)
-					ans += minCost;
-
-				minCost = Math.min(A[v], A[w]);
-				visited[v] = true;
-				visited[w] = true;
-				flag = true;
+		for(int i = 1; i <= N; i++) {
+			if(cost[i] != 10001) {
+				ans += cost[i];
+				System.out.println(cost[i] + "더함");
 			}
+			else if(i != parent[i])
+				continue;
 			else {
-				visited[v] = true;
-				visited[w] = true;
-				
-				minCost = Math.min(minCost, Math.min(A[v], A[w]));
-				
-				if(i == M-1) {
-					ans += minCost;
-					break;
-				}
-				flag = true;
-			}	
-			
+				ans += A[i];
+				System.out.println(A[i] + "기본값 더함");
+			}
 		}
 		
-		for(int i = 1; i <= N; i++)
-			if(visited[i] == false)
-				ans += A[i];
-		
-		if(ans <= k)
+		if(ans <= k) 
 			System.out.println(ans);
 		else
 			System.out.println("oh no");
 	}
 
 	public static void union(int x, int y) {
+		//int minCost = Math.min(A[x], A[y]);
 		x = find(x);
 		y = find(y);
-		
-		if(x == y)
-			return;
+
 		if(x < y) {
 			parent[y] = x;
+			//cost[x] = Math.min(cost[x], minCost);
 		}
-		else parent[x] = y;
+		else {
+			parent[x] = y;
+			//cost[y] = Math.min(cost[y], minCost);
+		}
 	}
 
 	public static int find(int x) {
@@ -104,24 +88,5 @@ public class b_16562 {
 			return x;
 		
 		return parent[x] = find(parent[x]);
-	}
-}
-
-class Friend implements Comparable<Friend>{
-	int x, y;
-	
-	public Friend(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	@Override
-	public int compareTo(Friend arg0) {
-		if(x > arg0.x && x > arg0.y)
-			return 1;
-		else if(y > arg0.x && y > arg0.y)
-			return 1;
-		else
-			return -1;
 	}
 }
