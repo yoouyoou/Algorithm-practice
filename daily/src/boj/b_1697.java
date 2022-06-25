5d6438687e7aea97d54604679a27cd42b698ef9b
유나연 <yny026@gmail.com>
2022-06-25 22:18:18 +0900

    백준 숨바꼭질 메모리 초과 수정

--- daily/src/boj/b_1697.java
+++ daily/src/boj/b_1697.java
@@ -7,64 +7,59 @@ import java.util.Scanner;
 //¼û¹Ù²ÀÁú
 public class b_1697 {
 
-	static int ans = 0, K;
-	static int[] visited = new int[100001];
+	static int ans = Integer.MAX_VALUE, K;
+	static int[] count = new int[100001];
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		K = sc.nextInt();
 		
-//		bfs(N, 0);
-		
         if(N == K)
             ans = 0;
         else
-		    bfs2(N);
+		    bfs(N);
         
 		System.out.println(ans);
 		sc.close();
 	}
 
-	public static void bfs2(int x) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x, 0});
+	public static void bfs(int x) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(x);
+		count[x] = 1;
 		
 		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			System.out.println(temp[0] + " "  + temp[1]);
+			int temp = q.poll();
 			
-			if(temp[0] == K) {
-				ans = temp[1];
+			System.out.println(temp + " "  + count[temp]);
+			
+			if(temp == K) {
+				ans = count[temp] - 1;
 				return;
 			}
 			
-			q.add(new int[] {temp[0] + 1, temp[1] + 1});
-			q.add(new int[] {temp[0] - 1, temp[1] + 1});
-			q.add(new int[] {temp[0] * 2, temp[1] + 1});
+			for(int i = 0; i < 3; i++) {
+				int next;
+				
+				if(i == 0)
+					next = temp + 1;
+				else if(i == 1)
+					next = temp - 1;
+				else
+					next = temp * 2;
+				
+				if(next >= 0 && next <= 100000 && count[next] == 0) {
+					count[next] = count[temp] + 1;
+					q.add(next);
+				}
+			}
+			
+//			q.add(new int[] {temp[0] + 1, temp[1] + 1});
+//			q.add(new int[] {temp[0] - 1, temp[1] + 1});
+//			q.add(new int[] {temp[0] * 2, temp[1] + 1});
 		}
 		
 	}
 	
-	/*
-	public static void bfs(int x, int count) {
-		System.out.println(x + ", " + count);
-		if(x == K) {
-			ans = count;
-			return;
-		}
-		
-		if(count > ans)
-			return;
-		
-		if(x * 2 < K)
-			bfs(x * 2, count+1);
-		else if(x < K) {
-			bfs(x + 1, count+1);
-			bfs(x * 2, count+1);
-		}
-		else
-			bfs(x-1, count+1);
-	}
-	*/
 }
