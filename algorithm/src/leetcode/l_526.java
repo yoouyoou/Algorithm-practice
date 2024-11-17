08c8ba6614a10d2dbb3f3b8b2b87ef864fdc8ece
유나연 <yny026@gmail.com>
2024-11-17 19:13:28 +0900

    20241117_Beautiful Arrangement_백트래킹

--- algorithm/src/leetcode/l_526.java
+++ algorithm/src/leetcode/l_526.java
@@ -3,50 +3,34 @@ package leetcode;
 public class l_526 {
 
 	static int answer;
+	static boolean[] visited;
 	
 	public static void main(String[] args) {
 		System.out.println(countArrangement(2));
 	}
 	
 	public static int countArrangement(int n) {
-        answer = 0;
-        int[] arr = new int[n];
-        int[] output = new int[n];
-        boolean[] visited = new boolean[n];
-        
-        for(int i = 1; i <= n; i++)
-        	arr[i-1] = i;
-        perm(arr, output, visited, 0, n);
+		answer = 0;
+		visited = new boolean[n+1];
+        DFS(n, 1, new int[n+1]);
         return answer;
     }
 	
-	public static void perm(int[] arr, int[] output, boolean[] visited, int depth, int n) {
-		if(depth == n) {
-			// 아름다운 배열인지 체크 output
-			for(int i: output)
-				System.out.print(i +" ");
-			System.out.println();
-			if(checkBeautifulArr(output)) answer++;
+	public static void DFS(int n, int depth, int[] perm) {
+		if(depth == n+1) {
+			answer++;
 			return;
 		}
 		
-		for(int i= 0; i < n; i++) {
-			if(visited[i] != true) {
-				visited[i] = true;
-				output[depth] = arr[i];
-				perm(arr, output, visited, depth + 1, n);
-				visited[i] = false;
+		for(int i = 1; i <= n; i++) {
+			if(!visited[i]) {
+				if(i % depth == 0 || depth % i == 0) {
+					perm[depth] = i;
+					visited[i] = true;
+					DFS(n, depth+1, perm);
+					visited[i] = false;
+				}
 			}
 		}
-	}
-	
-	public static boolean checkBeautifulArr(int[] arr) {
-		for(int i = 1; i <= arr.length; i++) {
-			if(!(i % arr[i-1] == 0 || arr[i-1] % i == 0)) {
-				return false;
-			}
-		}
-		
-		return true;
 	}
 }
